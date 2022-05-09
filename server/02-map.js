16aaf7921779940f1bf00f3129e70e6d408fab81
Luisa Franco <lmfj95@gmail.com>
2022-05-09 17:56:49 -0500

    Class 2: Map

---
+++ server/02-map.js
@@ -0,0 +1,18 @@
+const letters = ['a', 'b', 'c'];
+
+const newArray= [];
+for (let index = 0 ;  index < letters.length; index ++) {
+   const element  =letters[index];
+   newArray.push(element + '++');
+}
+
+console.log('original' , letters);
+
+console.log('new' , newArray);
+
+
+//MAP
+
+const newArrayMap = letters.map(item => item + '++');
+
+console.log(newArrayMap);
